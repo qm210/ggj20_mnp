c25827150bd8e8f5d653643dbc04a7a8383f8001
qm210 <quantenmace@gmail.com>
2020-02-01 15:45:52 +0100

    implementing filebrowser as nr4 arrived

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ var config = {
     width: 1600,
     height: 900,
     backgroundColor: 0x000000,
-    scene: [Main, Firewall],
+    scene: [Main, FileBrowser, Firewall],
     type: Phaser.WEBGL,
 }
 
